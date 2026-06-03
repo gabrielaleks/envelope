4a90494420c152b0752b0d4a9a07df35d9b356e9
Gabriel Aleksandravicius <gabriel.aleks19@gmail.com>
2026-06-03 20:08:09 +0200

    Add lora config

---
+++ shared/lora_config.h
@@ -0,0 +1,16 @@
+static const int PIN_LORA_CS = 7;
+static const int PIN_LORA_DIO0 = 9;
+static const int PIN_LORA_DIO1 = 33;
+static const int PIN_LORA_RST = 8;
+static const int PIN_LORA_MOSI = 6;
+static const int PIN_LORA_MISO = 3;
+static const int PIN_LORA_SCLK = 5;
+static const float LORA_FREQUENCY = 868.0;
+static const float LORA_BANDWIDTH = 125.0;
+static const int LORA_SPREADING_FACTOR = 12;
+static const int LORA_CODING_RATE = 6;
+static const int LORA_SYNC_WORD = 0xAB;
+static const int LORA_OUTPUT_POWER = 14;
+static const int LORA_CURRENT_LIMIT = 140;
+static const int LORA_PREAMBLE_LENGTH = 16;
+static const int LORA_RCR = false;
