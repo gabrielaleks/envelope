cd32e45c9a551ec05d2c4023e7290cb187435aa7
Gabriel Aleksandravicius <gabriel.aleks19@gmail.com>
2026-06-03 23:36:55 +0200

    Add display to receiver

--- receiver/src/main.cpp
+++ receiver/src/main.cpp
@@ -1,6 +1,32 @@
 #include <Arduino.h>
 
+#include "Display.h"
+#include "LoRaRadio.h"
+#include "packet.h"
+
+LoRaRadio _lora;
+Display _display;
+
 void setup() {
+    Serial.begin(115200);
+    delay(2000);
+
+    _lora.init();
+    _display.init();
 }
+
 void loop() {
+    Packet packet;
+    int state = _lora.receive(packet);
+
+    if (state == RADIOLIB_ERR_NONE) {
+        _display.clear();
+        Serial.println("Packet received:");
+        debugPrint(packet);
+        _display.println("Packet received");
+
+        Ack ack = {.seq_number = packet.seq_number, .success = true};
+        _lora.send(ack);
+        _display.println("ACK sent");
+    }
 }
