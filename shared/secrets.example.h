63ae933929ba79d2930534bc731dc41abbf8c0b1
Gabriel Aleksandravicius <gabriel.aleks19@gmail.com>
2026-06-17 22:00:06 +0200

    Add more MQTT secrets to example

--- shared/secrets.example.h
+++ shared/secrets.example.h
@@ -1,4 +1,7 @@
 #define WIFI_SSID "ssid"
 #define WIFI_PASSWORD "password"
-#define MQTT_BROKER "192.168.1.x"
+
+#define MQTT_USERNAME "username"
+#define MQTT_PASSWORD "password"
+#define MQTT_BROKER "192.168.178.37"
 #define MQTT_PORT 1883
